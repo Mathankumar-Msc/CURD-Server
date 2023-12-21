49009dab81975232556ff7e4f68cfeafc9407996
Mathankumar-Msc <129595945+Mathankumar-Msc@users.noreply.github.com>
2023-12-21 17:50:02 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Mathan:Mathan123@cluster0.slq6nhk.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Mathan:Mathan123@cluster0.slq6nhk.mongodb.net/mathan')
 .then(()=>{
     console.log("DB connected");
 }).catch(()=>{
